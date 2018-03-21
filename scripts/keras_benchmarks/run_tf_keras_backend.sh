ec41334f0d50ef72cf173022583cd9608eb78780
Lai Wei <royweilai@gmail.com>
2018-03-20 22:44:45 -0700

    update ignore

---
+++ scripts/keras_benchmarks/run_tf_keras_backend.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Keras Tensorflow Backend
+python -c "from keras import backend"
+KERAS_BACKEND=tensorflow
+sed -i -e 's/"backend":[[:space:]]*"[^"]*/"backend":\ "'$KERAS_BACKEND'/g' ~/.keras/keras.json;
+echo -e "Running tests with the following config:\n$(cat ~/.keras/keras.json)"
+
+# Use "cpu_config", "gpu_config" and "multi_gpu_config" as command line arguments to load the right
+# config file.
+#models='cifar10_cnn gru lstm mnist_mlp resnet50 vgg16 xception'
+models='resnet50_tf_keras'
+dir=`pwd`
+for name in $models
+do
+  python $dir/run_benchmark.py  --pwd=$dir --mode="$1" --model_name="$name" --dry_run=True
+done
+#!/usr/bin/env bash
